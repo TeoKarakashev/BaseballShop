490214f23dd33621451c3cf72cc3a2c0dfc7b3a7
Teodor <teodor.karakashev@gmail.com>
2021-04-01 11:13:53 +0300

    Favicon Interceptor

--- src/main/java/com/softuni/config/WebConfiguration.java
+++ src/main/java/com/softuni/config/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.softuni.config;
 
+import com.softuni.web.interceptor.FaviconInterceptor;
 import com.softuni.web.interceptor.PageTitleInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,13 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final PageTitleInterceptor pageTitleInterceptor;
+    private final FaviconInterceptor faviconInterceptor;
 
-    public WebConfiguration(PageTitleInterceptor pageTitleInterceptor) {
+    public WebConfiguration(PageTitleInterceptor pageTitleInterceptor, FaviconInterceptor faviconInterceptor) {
         this.pageTitleInterceptor = pageTitleInterceptor;
+        this.faviconInterceptor = faviconInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(pageTitleInterceptor);
+        registry.addInterceptor(this.pageTitleInterceptor);
+        registry.addInterceptor(this.faviconInterceptor);
     }
 }
